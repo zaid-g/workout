c9c5846317335d4409517a03febb386593aa284e
z g <z.gharaybeh@gmail.com>
2023-06-08 03:01:17 -0700

    working

---
+++ main.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+import datetime
+from matplotlib import pyplot as plt
+
+
+hist = pd.read_csv("history.csv")
+
+exercises = {0: "pushups", 1: "pullups", 2: "squats", 3: "jumprope"}
+
+exercise = "pushups"
+today = datetime.date.today()
+while True:
+    x = input(
+        f"""
+
+        Input reps for ***{exercise.upper()}*** in this format e.g. for person z: `z5,10,3,4`
+        To change exercise, enter the workout number: {exercises}.
+        Enter 'q' to quit.
+
+        """
+    )
+    if x == "q":
+        break
+    if len(x) <= 1:
+        try:
+            exercise = exercises[int(x)]
+        except Exception as exc:
+            print(f"Invalid input: {exc}")
+    else:
+        person = x[0]
+        reps = x[1:]
+        try:
+            reps = reps.split(",")
+            reps = [int(rep) for rep in reps]
+            for rep in reps:
+                row = {
+                    "date": today,
+                    "person": person,
+                    "exercise": exercise,
+                    "reps": rep,
+                }
+                hist.loc[len(hist)] = row
+        except Exception as exc:
+            print(f"Invalid format: {exc}")
+
+hist.to_csv("history.csv", index=False)
+
+# %% -------- [calculate score for each person and exercise] ----------:
+
+scores = pd.DataFrame(columns=["date", "person", "exercise", "score", "score_type"])
+
+grouped = list(hist.groupby(["person", "exercise", "date"]))
+grouped = [group.reset_index() for _, group in grouped]
+
+for group in grouped:
+    rms = sum(group.reps**2) ** 0.5
+    sum_ = sum(group.reps)
+    scores.loc[len(scores)] = {
+        "date": group.iloc[0].date,
+        "person": group.iloc[0].person,
+        "exercise": group.iloc[0].exercise,
+        "score": rms,
+        "score_type": "RMS",
+    }
+    scores.loc[len(scores)] = {
+        "date": group.iloc[0].date,
+        "person": group.iloc[0].person,
+        "exercise": group.iloc[0].exercise,
+        "score": sum_,
+        "score_type": "sum",
+    }
+
+
+# %% -------- [plot results] ----------:
+
+subplots = scores.groupby(["person", "exercise"])
+subplots = [group.reset_index() for _, group in subplots]
+
+fig, ax = plt.subplots(
+    nrows=int(len(subplots) ** 0.5 + 1), ncols=int(len(subplots) ** 0.5 + 1)
+)
+ax_flat = ax.flatten()
+
+i = 0
+for subplot in subplots:
+    subsubplots = subplot.groupby("score_type")
+    subsubplots = [group.reset_index() for _, group in subsubplots]
+    for subsubplot in subsubplots:
+        x = subsubplot.date
+        y = subsubplot.score
+        label = subsubplot.iloc[0].score_type
+        ax_flat[i].plot(x, y,  '-.', label=label)
+        ax_flat[i].set_ylabel("score")
+        ax_flat[i].title.set_text(
+            f"{subplot.iloc[0].person}, {subplot.iloc[0].exercise}"
+        )
+        ax_flat[i].legend(loc="upper right")
+    i += 1
+
+plt.show()
