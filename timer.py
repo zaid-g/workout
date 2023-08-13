17b75f5fe6fa23711a0d77a97e6f6e20da997edd
z g <z.gharaybeh@gmail.com>
2023-08-12 22:17:32 -0700

    F

--- timer.py
+++ timer.py
@@ -2,7 +2,7 @@ import os
 import time
 
 def start_set():
-    for i in range(3):
+    for i in range(2):
         os.system("paplay /usr/share/sounds/freedesktop/stereo/complete.oga&")
         time.sleep(0.3)
 
@@ -11,18 +11,20 @@ def start_exercise():
         os.system("paplay /usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga&")
         time.sleep(0.3)
 
-#workout = [["pullups", 4, 4], ["pushups", 4, 4], ["squats/planks", 4, 2]] # debug
 workout = [["pullups", 180, 5], ["pushups", 180, 5], ["squats/planks", 180, 2]]
+# workout = [["pullups", 4, 2], ["pushups", 4, 2], ["squats/planks", 4, 1]] # debug
 
-time.sleep(20)
+time.sleep(30)
 
 new_exercise_flag = True
-for exercise in workout:
+for i, exercise in enumerate(workout):
     while exercise[2] > 0:
         if not new_exercise_flag:
             start_set()
         else:
             start_exercise()
+        if i == len(workout) - 1 and exercise[2] == 1:
+            exit()
         time.sleep(exercise[1])
         exercise[2] -= 1
         new_exercise_flag = False
