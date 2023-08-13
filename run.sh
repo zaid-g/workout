e335c21705a50d11a8e3004fb0a26245a99df791
z g <z.gharaybeh@gmail.com>
2023-08-13 12:45:50 -0700

    F

--- run.sh
+++ run.sh
@@ -1,1 +1,21 @@
-python3 timer.py & python3 sets.py
+#!/bin/bash
+
+# Run the background job in the background (timer application)
+python3 timer.py &
+
+# Store the background job's process ID (PID)
+bg_pid=$!
+
+# Define a function to clean up the background job
+cleanup() {
+    echo "Ctrl+C detected."
+    echo "Canceling the timer program..."
+    kill -TERM $bg_pid
+    exit 1
+}
+
+# Set up a trap to call the cleanup function on Ctrl+C
+trap cleanup INT
+
+# Run the foreground job
+python3 sets.py
